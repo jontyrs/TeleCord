726ffd652af2692587353aa94b8d5ddc943f366a
Rachnit Singh Jonty <jontyrs99@gmail.com>
2020-08-28 15:25:47 +0530

    update app.listen

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ io.on('connection', socket => {
 
 
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
     // console.log(`Server started on port http://localhost:${PORT}`);
 });
